ba0ea9a4397872d114c18fb7c916f43a4fec0ce6
fondoger <fondoger@outlook.com>
2025-06-23 16:33:56 +0800

    Merge branch 'master' into postgres-dev (#19)

--- tools/dbutils/json.go
+++ tools/dbutils/json.go
@@ -14,7 +14,7 @@ import (
 // some normalizations for non-json columns.
 func JSONEach(column string) string {
 	// note: we are not using the new and shorter "if(x,y)" syntax for
-	// compatability with custom drivers that use older SQLite version
+	// compatibility with custom drivers that use older SQLite version
 	/* SQLite:
 	return fmt.Sprintf(
 		`json_each(CASE WHEN iif(json_valid([[%s]]), json_type([[%s]])='array', FALSE) THEN [[%s]] ELSE json_array([[%s]]) END)`,
@@ -53,7 +53,7 @@ func JsonArrayExistsStr(column string, strValue string) dbx.Expression {
 // Returns 0 for empty string or NULL column values.
 func JSONArrayLength(column string) string {
 	// note: we are not using the new and shorter "if(x,y)" syntax for
-	// compatability with custom drivers that use older SQLite version
+	// compatibility with custom drivers that use older SQLite version
 	/* SQLite:
 	return fmt.Sprintf(
 		`json_array_length(CASE WHEN iif(json_valid([[%s]]), json_type([[%s]])='array', FALSE) THEN [[%s]] ELSE (CASE WHEN [[%s]] = '' OR [[%s]] IS NULL THEN json_array() ELSE json_array([[%s]]) END) END)`,
